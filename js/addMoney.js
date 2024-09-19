7e2ca826dff9869ad6c95c85f2baf06835f9da2f
satyajit <satyajit_jbl@yahoo.com>
2024-09-19 22:02:23 +0600

    first commit

---
+++ js/addMoney.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+   
+    const addMoney = document.getElementById('input-add-money').value;
+    const addMoneyNumber = parseFloat(addMoney);
+    const pinNumber = document.getElementById('input-pin-number').value;
+    
+    console.log(addMoney, pinNumber);
+    
+    if(pinNumber === "1234"){
+        const balance = document.getElementById('account-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber + addMoneyNumber;
+        document.getElementById('account-balance').innerText = newBalance;
+        console.log(newBalance);
+    }
+    else{
+        alert('wrong pin');
+    }
+    
+})
